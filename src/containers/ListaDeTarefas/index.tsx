93f973395f3aaf1a636cdcf675eb555c16c45d49
Algayer <nairjoaocarlos@gmail.com>
2026-01-28 14:22:47 -0300

    finalizado aula 5 e 6

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,7 +1,37 @@
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+
+const tarefas = [
+  {
+    titulo: 'Tarefa 1',
+    prioridade: 'Alta',
+    status: 'Pendente',
+    descricao: 'Descrição da tarefa 1'
+  },
+  {
+    titulo: 'Tarefa 2',
+    prioridade: 'Média',
+    status: 'Em andamento',
+    descricao: 'Descrição da tarefa 2'
+  }
+]
+
 const ListaDeTarefas = () => (
-  <main>
+  <Container>
     <p>2 tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
-  </main>
+    <ul>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
+    </ul>
+  </Container>
 )
 
 export default ListaDeTarefas
